1ef0e5f804d68354755b670a74a9f34a1eed3f41
chinyue1910 <asd874631@gmail.com>
2020-09-01 22:35:15 +0800

    圖片改成可以拖曳

--- src/components/avatar.vue
+++ src/components/avatar.vue
@@ -1,23 +1,31 @@
 <template lang="pug">
   #avatar
-    label#circle(for="input") 請選擇檔案
+    .dropzone-area(
+      v-if="!image"
+      drag-over="handleDragOver"
+      @dragenter="dragging=true"
+      @dragleave="dragging=false")
+      .dropzone-text
+        span(class="dropzone-title") 拖曳或是選取圖片
+        span(class="dropzone-info" v-if="help") help
+      input(type="file" @change="onFileChange")
+    .dropzone-preview
       img(:src="image")
-    input#input(type="file" @change="onFileChange")
+      button(@click="removeImage" v-if="image") Remove
 </template>
 
 <script>
 export default {
   data () {
     return {
-      image: null
+      image: '',
+      dragging: false
     }
   },
   methods: {
     onFileChange (e) {
       const files = e.target.files || e.dataTransfer.files
-      if (!files.length) {
-        return
-      }
+      if (!files.length) return
       this.createImage(files[0])
     },
     createImage (file) {
@@ -37,25 +45,69 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#circle{
-  width: 200px;
-  height: 200px;
-  border-radius: 50%;
-  border: 1px solid gray;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  position: relative;
-  color: gray;
+.dropzone-area {
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    position: relative;
+    border: 2px dashed #CBCBCB;
+    &:hover {
+        border: 2px dashed #2E94C4;
+        .dropzone-title {
+          color: #1975A0;
+        }
+    }
 }
-#input{
-  display: none;
+.dropzone-area input {
+    position: absolute;
+    cursor: pointer;
+    top: 0px;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
 }
-img{
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
+.dropzone-text {
+    position: absolute;
+    top: 50%;
+    text-align: center;
+    transform: translate(0, -50%);
+    width: 100%;
+    span {
+        display: block;
+        font-family: Arial, Helvetica;
+        line-height: 1.9;
+    }
+}
+.dropzone-title {
+    font-size: 13px;
+    color: #787878;
+    letter-spacing: 0.4px;
+}
+.dropzone-info {
+    font-size: 13px;
+    font-size: 0.8125rem;
+    color: #A8A8A8;
+    letter-spacing: 0.4px;
+}
+.dropzone-button {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    display: none;
+}
+.dropzone-preview {
+    width: 80%;
+    position: relative;
+    &:hover .dropzone-button {
+        display: block;
+    }
+    img {
+      display: block;
+      height: auto;
+      max-width: 100%;
+    }
 }
 </style>
