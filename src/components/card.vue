29991f4144337fe6fee0f0a0f5fe4e098995bd6c
Rhapsody0128 <rhapsody0128@gmail.com>
2020-09-03 00:38:56 +0800

    card 暫時完成

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,41 +1,45 @@
 <template lang="pug">
   #card
-    .card(v-for="(card,index) in sortedData" :key="index" @click='flip(index)')
-      .surface.surface-0
-        .img
-          slot(:name="'img-'+index")
-            slot(name="img")
-              img(:src="card.img")
-        .body
-          .title(:style="card.titleColor")
-            slot(:name="'title-'+index")
-              slot(name="title")
-                h1 {{card.title}}
-          .text(:style="card.textColor")
-            slot(:name="'text-'+index")
-              slot(name='text')
-                p {{card.text}}
-      .surface(:class="'surface-'+(inx+1)" v-for="(child,inx) in card.children" :key='inx')
-        .img
-          slot(:name="'img-'+index")
-            slot(name="img")
-              img(:src="child.img")
-        .body
-          .title(:style="child.titleColor")
-            slot(:name="'title-'+index")
-              slot(name="title")
-                h1 {{child.title}}
-          .text(:style="child.textColor")
-            slot(:name="'text-'+index")
-              slot(name='text')
-                p {{child.text}}
-
+    .block(:style='blockStyle' v-for="(card,index) in sortedData" :key="index")
+      .card(:style="cardStyle"  @click='flip(card)')
+        .surface(v-if='stereo' v-for="(child,inx) in card.children" :key='inx'  :style="child.surface")
+          .img
+            slot(:name="'childImg-'+inx")
+              slot(name="img")
+                img(:src="child.img")
+          .body
+            .title(:style="child.titleColor")
+              slot(:name="'childTitle-'+inx")
+                slot(name="title")
+                  h1 {{child.title}}
+            .text(:style="child.textColor")
+              slot(:name="'childText-'+inx")
+                slot(name='text')
+                  p {{child.text}}
+        .surface(:style="card.surface")
+          .img
+            slot(:name="'img-'+index")
+              slot(name="img")
+                img(:src="card.img")
+          .body
+            .title(:style="card.titleColor")
+              slot(:name="'title-'+index")
+                slot(name="title")
+                  h1 {{card.title}}
+            .text(:style="card.textColor")
+              slot(:name="'text-'+index")
+                slot(name='text')
+                  p {{card.text}}
 </template>
 <script>
 export default {
   props: {
     titleColor: String,
     textColor: String,
+    stereo: Boolean,
+    width: { type: Number, default: 400 },
+    height: { type: Number, default: 500 },
+    backgroundColor: { type: String, default: '#F7F6EE' },
     data: {
       type: Array,
       default: function () {
@@ -46,12 +50,33 @@ export default {
         }]
       }
     }
-
+  },
+  data () {
+    return {
+      cardStyle: {},
+      blockStyle: {}
+    }
   },
   computed: {
     sortedData () {
       const sortedData = this.data
       sortedData.forEach(perData => {
+        perData.surface = {
+          background: this.backgroundColor
+        }
+        if (perData.children !== undefined) {
+          const distnce = (this.width / 2) * Math.tan([90 * (perData.children.length + 1) - 180] / (perData.children.length + 1) * [Math.PI / 180])
+          perData.page = 0
+          perData.surface = {
+            transform: `rotateY(0deg) translateZ(${distnce}px)`,
+            background: this.backgroundColor
+          }
+          let i = 0
+          perData.children.forEach(child => {
+            i++
+            child.surface = { transform: `rotateY(${360 / (perData.children.length + 1) * (i)}deg) translateZ(${distnce}px)`, background: this.backgroundColor }
+          })
+        }
         if (perData.titleColor !== undefined) {
           perData.titleColor = { color: perData.titleColor }
         } else {
@@ -71,48 +96,57 @@ export default {
           }
         }
       })
-      console.log(sortedData)
       return sortedData
     }
   },
   methods: {
-    flip (index) {
-      console.log(index)
+    flip (card) {
+      if (this.stereo) {
+        card.page--
+        if (card.children !== undefined) {
+          const level = card.children.length + 1
+          this.cardStyle = {
+            transform: `rotateY(${(360 / level) * card.page}deg)`,
+            width: this.width + 'px',
+            height: this.height + 'px'
+          }
+        }
+      }
     }
   },
   mounted () {
+    this.cardStyle = {
+      width: this.width + 'px',
+      height: this.height + 'px'
+    }
+    this.blockStyle = {
+      width: this.width + 'px',
+      height: this.height + 'px'
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.card{
-  width: 400px;
-  height: 500px;
-  transition: 1s;
+
+.block{
   margin: 1rem;
+  overflow: hidden;
+  border-radius: 2rem;
+  display: inline-block;
+  box-shadow: 1rem 1rem 1rem rgba(0,0,0,0.1);
+}
+.card{
+  transition: 1s;
   position: relative;
   display: inline-block;
   transform-style: preserve-3d;
-  transform: rotateY(0deg);
-  .surface-0{
-    transform: rotateY(0deg) translateZ(50px);
-  }
-  .surface-1{
-    transform: rotateY(180deg) translateZ(200px);
-  }
+
   .surface{
-    box-shadow: 1rem 1rem 1rem rgba(0,0,0,0.5);
     width: 100%;
     height: 100%;
     position: absolute;
     overflow: hidden;
     border-radius: 2rem;
-    background: #EAE6DA;
-    margin: 1rem;
-    top :0;
-    right :0;
-    left :0;
-    bottom :0;
     .img{
       margin: 0;
       width:100%;
@@ -132,9 +166,8 @@ export default {
 }
 .card:hover{
   img{
-    transform:scale(1.3,1.3);
+    transform: scale(1.3,1.3);
   }
-  transform: rotateY(180deg);
 }
 
 </style>
--- src/views/Card.vue
+++ src/views/Card.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 #show
-  m-card(:data='cards')
-    template(#title)
-      h1 全都給我改
+  m-card(:data='card'
+  )
     template(#text-0)
       p 只給我改第一個
   m-card(
     titleColor='gray'
     textColor='black'
+    backgroundColor="#"
   )
     template(#title)
       img(src='/images/moudou.jpg')
@@ -22,13 +22,6 @@ export default {
     return {
       cards: [
         {
-          title: '毛豆家',
-          text: '毛豆很好吃，毛豆真的很好吃，所以來使用毛豆的卡片。',
-          img: '/images/moudou.jpg',
-          titleColor: 'red',
-          textColor: 'blue'
-        },
-        {
           title: '第一頁',
           text: '毛豆很好吃，毛豆真的很好吃，所以來使用毛豆的卡片。',
           img: '/images/moudou.jpg',
@@ -37,8 +30,50 @@ export default {
               title: '第二夜',
               text: '毛豆很好吃。',
               img: '/images/moudou.jpg'
+            },
+            {
+              title: '第三夜',
+              text: '毛豆很膩。',
+              img: '/images/moudou.jpg'
+            },
+            {
+              title: '第四夜',
+              text: '毛豆很膩。',
+              img: '/images/moudou.jpg'
+            },
+            {
+              title: '第五夜',
+              text: '毛豆很膩。',
+              img: '/images/moudou.jpg'
             }
+            // {
+            //   title: '第六夜',
+            //   text: '毛豆很膩。',
+            //   img: '/images/moudou.jpg'
+            // }
+            // {
+            //   title: '第七夜',
+            //   text: '毛豆很膩。',
+            //   img: '/images/moudou.jpg'
+            // }
           ]
+        }
+      ],
+      card: [
+        {
+          title: '第一頁',
+          text: '毛豆很好吃，毛豆真的很好吃，所以來使用毛豆的卡片。',
+          img: '/images/moudou.jpg'
+        },
+        {
+          title: '第一頁',
+          text: '毛豆很好吃，毛豆真的很好吃，所以來使用毛豆的卡片。',
+          img: '/images/moudou.jpg'
+        },
+        {
+          title: '第一頁',
+          text: '毛豆很好吃，毛豆真的很好吃，所以來使用毛豆的卡片。',
+          img: '/images/moudou.jpg'
         }
       ]
     }
