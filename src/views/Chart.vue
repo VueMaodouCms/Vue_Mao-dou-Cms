b7d46d8ed2379e63f476e628c7e62235ce385c6a
chinyue1910 <asd874631@gmail.com>
2020-09-04 21:09:30 +0800

    等待修正

--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,36 +1,48 @@
 <template lang="pug">
 #show
-  m-chart(
+  //- m-chart(
+  //-   :width="400",
+  //-   :height="300",
+  //-   chartStyle="barchart",
+  //-   :detail="boxs",
+  //-   :colors="color",
+  //-   :gridScale="5"
+  //-   label
+  //- )
+  //- m-chart(
+  //-   :width="400",
+  //-   :height="300",
+  //-   chartStyle="piechart",
+  //-   :detail="boxs",
+  //-   :doughnutHoleSize="0.5",
+  //-   :colors="color",
+  //-   textcolor="white",
+  //-   label
+  //- )
+  //- m-chart(
+  //-   :width="400",
+  //-   :height="300",
+  //-   chartStyle="linechart",
+  //-   :detail="datas",
+  //-   :colors="color",
+  //-   label,
+  //-   :gridScale="50"
+  //- )
+  test(
     :width="400",
     :height="300",
-    chartStyle="barchart",
-    :detail="boxs",
-    :colors="color",
-    :gridScale="5"
-    label
+    chartName="bar",
+    :detail="asd",
+    :feilds="feilds"
   )
-  m-chart(
-    :width="400",
-    :height="300",
-    chartStyle="piechart",
-    :detail="boxs",
-    :doughnutHoleSize="0.5",
-    :colors="color",
-    textcolor="white",
-    label
-  )
-  m-chart(
-    :width="400",
-    :height="300",
-    chartStyle="linechart",
-    :detail="datas",
-    :colors="color",
-    label,
-    :gridScale="50"
-  )
+
 </template>
 <script>
+import test from '../components/test'
 export default {
+  components: {
+    test
+  },
   data () {
     return {
       boxs: [
@@ -40,38 +52,17 @@ export default {
         { name: '討債', value: 8 }
       ],
       color: ['red', 'blue', 'green', 'black', '#EAE6DA', '#F7F6EE'],
-      datas: [
+      asd: [
         {
           name: '雞排',
-          value: {
-            first: 50,
-            second: 150,
-            third: 170,
-            forth: 150,
-            fifth: 50
-          }
+          value: [50, 100, 150, 200, 250]
         },
         {
-          name: '飲料',
-          value: {
-            first: 100,
-            second: 120,
-            third: 200,
-            forth: 180,
-            fifth: 200
-          }
-        },
-        {
-          name: '娛樂',
-          value: {
-            first: 30,
-            second: 80,
-            third: 77,
-            forth: 80,
-            fifth: 370
-          }
+          name: '珍奶',
+          value: [500, 250, 200, 150, 100]
         }
-      ]
+      ],
+      feilds: ['我', '是', '大', '帥', '哥']
     }
   }
 }
